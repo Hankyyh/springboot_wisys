61f5617145c8f108dd8f1df97d4c672d9d796be7
Weiheng Ni <weihengn@gmail.com>
2021-04-11 14:26:50 -0700

    add project files

---
+++ src/main/java/com/nebula/wisys/WisysApplication.java
@@ -0,0 +1,13 @@
+package com.nebula.wisys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WisysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WisysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nebula/wisys/WisysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nebula.wisys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WisysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
